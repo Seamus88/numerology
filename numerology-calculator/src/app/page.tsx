63983b8f762bf79010477da9a308651730090a79
James Cosgrove <Seamus.Coz@gmail.com>
2025-02-28 02:21:05 +0000

    Added hooks for Name and Birthday input

--- numerology-calculator/src/app/page.tsx
+++ numerology-calculator/src/app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 
 export default function Home() {
+  const [fullName, setfullName] = useState("");
   const [birthday, setBirthday] = useState("");
 
   return (
@@ -11,13 +12,8 @@ export default function Home() {
       <Directions/>
       <div className="flex flex-col items-center gap-8">
         <div className="flex flex-col items-center gap-4">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="p-2 border border-gray-300 rounded-md"
-          />
+          <NameInput fullName={fullName} setfullName={setfullName}/>
+          {fullName && <p>Your selected name is: {fullName}</p>}
           <BirthdayInput birthday={birthday} setBirthday={setBirthday}/>
           {birthday && <p>Your selected birthday is: {birthday}</p>}
         </div>
@@ -52,8 +48,30 @@ function BirthdayInput({ birthday, setBirthday}) {
         className="p-2 border border-gray-300 rounded-md"
         value={birthday}
         onChange={(e) => setBirthday(e.target.value)}
+      />     
+    </div>
+  );
+}
+
+function NameInput({ fullName, setfullName}) {
+
+  function handleClick(fullNameElement: HTMLInputElement) {
+    setfullName(fullNameElement.value);
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <label htmlFor="fullName">Full Name</label>
+      <input 
+        type="text" 
+        id="fullname" 
+        name="fullname"
+        className="p-2 border border-gray-300"
+        defaultValue=""
       />
-      
+      <button 
+        id="namebutton" 
+        className="border border-black-300 rounded-md" onClick={() => handleClick(document.getElementById("fullname"))}>Change Name</button>
     </div>
   );
 }
